cad302ea306ead9a7eea5dbd1760137cb8142194
Florian.Popa <florian.popa@wolfpack-digital.com>
2018-08-14 17:06:16 +0300

    First day : https://trello.com/c/ptkgBASx/24-assignment-2

--- EurekaCaffe/config/routes.rb
+++ EurekaCaffe/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'user/new'
 
-  root 'static_pages#home'
+  root 'layouts#application'
   get 'about', to: 'static_pages#about'
 end
